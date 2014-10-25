daa91fe53f21b236cb60323f009a616304805f3e
davidlguida@gmail.com <davidlguida@gmail.com>
2014-10-25 15:28:01 -0400

    finish login/logout (with 6 errors)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root             'static_pages#home'
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'faq'     => 'static_pages#faq'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
