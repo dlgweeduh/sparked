18a847f57e270321899bce0076983ffa6b028157
davidlguida@gmail.com <davidlguida@gmail.com>
2014-11-13 23:07:33 -0500

    add video

---
+++ app/assets/javascripts/hero.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	
+	$('.word-swap').typed({
+		strings: [ "Designers", "Thinkers","Creators"],
+		typeSpeed: 70, // typing speed
+           
+	});
+});
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class UserMailerTest < ActionMailer::TestCase
   
   test "account_activation" do
-    user = User.create(name: "Test User", email: "test@example.com",
+    user = User.create(name: "Test User", email: "test@sparkeducation.ca",
                        password: "foobar", password_confirmation: "foobar")
     mail = UserMailer.account_activation(user)
     assert_equal "Sparked Account Activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["noreply@sparkeducation.ca"], mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
@@ -20,7 +20,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["noreply@sparkeducation.ca"], mail.from
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
