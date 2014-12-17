dd5776b6904a4c992e82dda8467c0cff8059dcbb
davidlguida@gmail.com <davidlguida@gmail.com>
2014-12-16 21:11:38 -0500

    adds head partial w/ meta data, attempts at fonts

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,9 @@ module Sparked
     # -- all .rb files in that directory are automatically loaded.
 
     #~~ add app/assets/fonts to the asset path
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # app.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')  
+    # app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
