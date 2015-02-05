b00b1fa9ada60c2cd9c3a92d66b47735e96a53bb
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-05 12:54:20 -0500

    programs page has rounded out and is near production

--- app/assets/javascripts/secondary-nav.js
+++ app/assets/javascripts/secondary-nav.js
@@ -1,11 +1,22 @@
 
 $(document).ready(function() {
-	$('body').scrollspy({ target: '.sp-categories' });
+	// $('body').scrollspy({ target: '.sp-categories' });
+       
+       /* activate scrollspy menu */
+         var $body   = $(document.body);
+         var navHeight = $('.navbar').outerHeight(true) + 10;
+
+         $body.scrollspy({
+           target: '.sp-categories',
+           offset: navHeight
+         });	
     
     $("#secondNav").affix({
         offset: { 
-            top: 600 
+            top: 600,
+		    bottom: function () {
+		      return (this.bottom = $('.global-footer').outerHeight(true))
+		    }             
      	}
     });
-
 });
