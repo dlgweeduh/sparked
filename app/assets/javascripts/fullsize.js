51a95ba1448e9162c08b672c7166bead8c47f743
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-04 20:15:11 -0500

    adds fullsize js to about and community

--- app/assets/javascripts/fullsize.js
+++ app/assets/javascripts/fullsize.js
@@ -1,18 +1,18 @@
-// $(function(){
-//     var windowH = $(window).height();
-//     var wrapperH = $('#wrapper').height();
-//     if(windowH > wrapperH) {                            
-//         $('#wrapper').css({'height':($(window).height())+'px'});
-//     }                                                                               
-//     $(window).resize(function(){
-//         var windowH = $(window).height();
-//         var wrapperH = $('#wrapper').height();
-//         var differenceH = windowH - wrapperH;
-//         var newH = wrapperH + differenceH;
-//         var truecontentH = $('#truecontent').height();
-//         if(windowH > truecontentH) {
-//             $('#wrapper').css('height', (newH)+'px');
-//         }
+$(function(){
+    var windowH = $(window).height();
+    var wrapperH = $('#heroLanding, #heroLanding2, #videoLanding').height();
+    if(windowH > wrapperH) {                            
+        $('#heroLanding, #heroLanding2').css({'height':($(window).height())+'px'});
+    }                                                                               
+    $(window).resize(function(){
+        var windowH = $(window).height();
+        var wrapperH = $('#heroLanding, #heroLanding2, #videoLanding').height();
+        var differenceH = windowH - wrapperH;
+        var newH = wrapperH + differenceH;
+        var truecontentH = $('#heroContent, #heroContent2, #videoOverlay').height();
+        if(windowH > truecontentH) {
+            $('#heroLanding, #heroLanding2, #videoLanding').css('height', (newH)+'px');
+        }
 
-//     })          
-// });
+    })          
+});
