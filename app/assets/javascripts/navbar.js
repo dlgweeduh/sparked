1f753f2b4069a3f69804becf0a15f2b82e8dace3
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-06 18:17:20 -0500

    remove home page

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         $('.navbar-nav li a').css({"text-shadow": "1.5px 1.5px 3px #666"});
     $(window).scroll(function(){
         if ($(window).scrollTop() > 1){
-            $('.navbar-inverse').css({ "background-color": "rgba(255, 255, 255, 0.9)",
-      "transition": ".2s"});
+            $('.navbar-inverse').css({ "background-color": "#fff",
+      "transition": ".2s","box-shadow":"0px 0px 16px 0px rgba(50,50,50,0.3"});
             $('.navbar-nav li a').css({"font-weight":"300", "color":"#57B7DF", "text-shadow": "none"})
         } else if ($(window).scrollTop() < 1){
             $('.navbar-inverse').css({"background-color":"rgba(255, 255, 255, 0	)"});
