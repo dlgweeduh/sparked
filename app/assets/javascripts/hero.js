3405089a1ad51dc4e6804d5c8b48305006dbf91d
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-10 14:34:46 -0500

    stable -- making a master

--- app/assets/javascripts/hero.js
+++ app/assets/javascripts/hero.js
@@ -1,14 +1,23 @@
 
-var ready;
-ready = function() {
+
+$(document).ready(function() {
 	$('.word-swap').typed({
 		strings: [ "Thinkers.", "Creators.", "Sparked."],
 		typeSpeed: 70, // typing speed
            
-	});
-};
+	});   
+});   	
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+// var ready;
+// ready = function() {
+// 	$('.word-swap').typed({
+// 		strings: [ "Thinkers.", "Creators.", "Sparked."],
+// 		typeSpeed: 70, // typing speed
+           
+// 	});
+// };
+
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
 
 
