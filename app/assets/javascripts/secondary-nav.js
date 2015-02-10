79565cefa77bd25ca42d4da2a4e190da4ceb85fb
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-10 14:02:56 -0500

    stable -- just taking offline for a jiff

--- app/assets/javascripts/secondary-nav.js
+++ app/assets/javascripts/secondary-nav.js
@@ -1,25 +1,52 @@
 
-$(document).ready(function() {
-	// $('body').scrollspy({ target: '.sp-categories' });
-       
-       /* activate scrollspy menu */
+var ready;
+ready = function() {
          var $body   = $(document.body);
          var navHeight = $('.navbar').outerHeight(true) + 10;
 
          $body.scrollspy({
            target: '.sp-categories',
            offset: navHeight
-         });	
+         });  
     
     $("#secondNav").affix({
         offset: { 
             top: 600,
-		    bottom: function () {
-		      return (this.bottom = $('.global-footer').outerHeight(true))
-		    }             
-     	}
+        bottom: function () {
+          return (this.bottom = $('.global-footer').outerHeight(true))
+        }             
+      }
     });
     $('#accordion2').collapse({
       toggle: false
     })
 });
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
+// $(document).ready(function() {
+// 	// $('body').scrollspy({ target: '.sp-categories' });
+       
+//        /* activate scrollspy menu */
+//          var $body   = $(document.body);
+//          var navHeight = $('.navbar').outerHeight(true) + 10;
+
+//          $body.scrollspy({
+//            target: '.sp-categories',
+//            offset: navHeight
+//          });	
+    
+//     $("#secondNav").affix({
+//         offset: { 
+//             top: 600,
+// 		    bottom: function () {
+// 		      return (this.bottom = $('.global-footer').outerHeight(true))
+// 		    }             
+//      	}
+//     });
+//     $('#accordion2').collapse({
+//       toggle: false
+//     })
+// });
---
+++ app/assets/javascripts/typeform.js
@@ -0,0 +1,9 @@
+// var ready;
+// ready = function() {
+// var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()
+// };
+
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
+
+
