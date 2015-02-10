8e5d957fad88a80b268f5a47db29061c689b758c
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-10 14:44:59 -0500

    stable -- making a master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or NGINX will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -29,11 +29,11 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # ~~~
-  # config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   # ~~~
-  # config.assets.digest = false
+  config.assets.digest = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
