1ea6da72ea4991cef804021c29c730a3005f6e41
davidlguida@gmail.com <davidlguida@gmail.com>
2015-02-11 10:52:26 -0500

    ttnkers w/ navbar dropdown et al

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-        $('.navbar-nav li a').css({"text-shadow": "1.5px 1.5px 3px #666"});
+        $('.navbar-nav > li > a').css({"text-shadow": "1.5px 1.5px 3px #666"});
     $(window).scroll(function(){
         if ($(window).scrollTop() > 1){
             $('.navbar-inverse').css({ "background-color": "#fff", "transition": ".2s", "box-shadow":"0px 0px 16px 0px rgba(50,50,50,0.3)"});
